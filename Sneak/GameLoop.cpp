6ed4feb5285f10f74906d6499b93be72ab7ec1a2
Remi386 <kipcramil@gmail.com>
2022-02-24 14:20:31 +0300

    Impl input handler

--- Sneak/GameLoop.cpp
+++ Sneak/GameLoop.cpp
@@ -17,8 +17,34 @@ std::vector<uint16_t> gameField(FieldHeight* FieldWidth);
 
 void Play(sf::RenderWindow& window)
 {
-	for (int i = 0; i < 20; ++i) {
-		gameField[dis(mersenne)] = 1;
+	mKey key = HandleInput();
+	switch (key)
+	{
+	case mKey::MoveUp:
+		for (int i = 0; i < 20; ++i) {
+			gameField[dis(mersenne)] = 0;
+		}
+		break;
+	case mKey::MoveRight:
+		for (int i = 0; i < gameField.size(); ++i) {
+			gameField[i] = 1;
+		}
+		break;
+	case mKey::MoveDown:
+		for (int i = 0; i < gameField.size(); ++i) {
+			gameField[i] = 0;
+		}
+		break;
+	case mKey::MoveLeft:
+		for (int i = 0; i < FieldHeight; ++i) {
+			for (int j = 0; j < FieldWidth; ++j) {
+				if (i < FieldHeight / 2 && j < FieldWidth / 2) {
+					gameField[i * FieldWidth + j] = 0;
+				}
+			}
+		}
+		break;
 	}
+	
 	drawField(window, gameField);
 }
--- Sneak/InputHandler.cpp
+++ Sneak/InputHandler.cpp
@@ -3,5 +3,28 @@
 
 mKey HandleInput()
 {
-	return mKey::MoveDown;
+	static mKey CacheKey = mKey::MoveRight;
+
+	if(sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+	{
+		CacheKey = mKey::MoveUp;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+	{
+		CacheKey = mKey::MoveRight;
+	}
+	
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+	{
+		CacheKey = mKey::MoveDown;
+	}
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+	{
+		CacheKey = mKey::MoveLeft;
+	}
+
+	return CacheKey;
+
 }
