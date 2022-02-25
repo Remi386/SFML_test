e02b92125b2427efe4be8f3a36d56dc9827f3e4d
Remi386 <kipcramil@gmail.com>
2022-02-25 21:51:43 +0300

    Base movement

--- Sneak/Constants.h
+++ Sneak/Constants.h
@@ -10,14 +10,14 @@ constexpr int32_t RightBoundary = 350;
 constexpr int32_t UpperBoundary = 50;
 constexpr int32_t LowerBoundary = 350;
 //Game unit i32n real pixels
-constexpr int32_t CellWidth = 4;
-constexpr int32_t CellHeight = 4;
+constexpr int32_t CellWidth = 10;
+constexpr int32_t CellHeight = 10;
 
 constexpr int32_t FieldWidth = (RightBoundary - LeftBoundary) / CellWidth;
 constexpr int32_t FieldHeight = (LowerBoundary - UpperBoundary) / CellHeight;
 constexpr uint32_t fieldSize = FieldHeight * FieldWidth;
 
-constexpr uint32_t FPS = 30;
+constexpr uint32_t FPS = 15;
 constexpr uint32_t FontSize = 24;
 
 enum class mKey
